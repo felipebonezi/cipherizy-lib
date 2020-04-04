5e84a150a4470dd46274a65b3ad274a3a3569cd1
Felipe Bonezi <1410319+felipebonezi@users.noreply.github.com>
2020-04-04 14:46:30 -0300

    chore: :hammer: Refactor gradle build file.
    Signed-off-by: Felipe Bonezi <1410319+felipebonezi@users.noreply.github.com>

--- src/main/java/vc/com/justa/cipher/algorithm/AESCipher.java
+++ src/main/java/vc/com/justa/cipher/algorithm/AESCipher.java
@@ -17,8 +17,8 @@ import java.security.NoSuchAlgorithmException;
  * Advanced Encryption Standard implementation.
  *
  * <p>
- * The Advanced Encryption Standard (AES), also known by its original name Rijndael (Dutch pronunciation: [ˈrɛindaːl]),[3] is a specification for the encryption of electronic data established by the U.S. National Institute of Standards and Technology (NIST) in 2001.[4]
- * AES is a subset of the Rijndael block cipher[3] developed by two Belgian cryptographers, Vincent Rijmen and Joan Daemen, who submitted a proposal[5] to NIST during the AES selection process.[6] Rijndael is a family of ciphers with different key and block sizes.
+ * The Advanced Encryption Standard (AES), also known by its original name Rijndael,[3] is a specification for the encryption of electronic data established by the U.S. National Institute of Standards and Technology (NIST) in 2001.[4]
+ * AES is a subset of the block cipher[3] developed by two Belgian cryptographers, Vincent Rijmen and Joan Daemen, who submitted a proposal[5] to NIST during the AES selection process.[6] Rijndael is a family of ciphers with different key and block sizes.
  * For AES, NIST selected three members of the Rijndael family, each with a block size of 128 bits, but three different key lengths: 128, 192 and 256 bits.
  * AES has been adopted by the U.S. government and is now used worldwide. It supersedes the Data Encryption Standard (DES),[7] which was published in 1977. The algorithm described by AES is a symmetric-key algorithm, meaning the same key is used for both encrypting and decrypting the data.
  * </p>
