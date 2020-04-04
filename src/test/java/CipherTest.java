88ffd764bfcaae09ff46593d9cb3e58f4f0825bd
Felipe Bonezi <1410319+felipebonezi@users.noreply.github.com>
2020-04-04 16:40:05 -0300

    docs: :memo: Document test class.
    Signed-off-by: Felipe Bonezi <1410319+felipebonezi@users.noreply.github.com>

--- src/test/java/CipherTest.java
+++ src/test/java/CipherTest.java
@@ -7,44 +7,75 @@ import vc.com.justa.cipher.algorithm.CipherFactory;
 
 import java.nio.charset.StandardCharsets;
 
+/**
+ * Cipher class test.
+ */
 public class CipherTest {
 
+    /**
+     * Credit card number with 16 chars.
+     */
     private static final String CREDIT_CARD_NUMBER_16 = "6516600011112222";
+
+    /**
+     * Credit card number with 22 chars.
+     */
     private static final String CREDIT_CARD_NUMBER_22 = "6062000011112222333355";
+
+    /**
+     * Master key used in encryption and decryption mode.
+     * 16 bytes = 128 bits.
+     */
     private static final byte[] KEY = "0029959810000151".getBytes(StandardCharsets.UTF_8);
+
+    /**
+     * Vector salt used in encryption and decryption mode.
+     * 16 bytes = 128 bits.
+     */
     private static final byte[] SALT = "JUSTA-PAGAMENTOS".getBytes(StandardCharsets.UTF_8);
 
-    private ICipher cipher;
-
-    @Before
-    public void before() {
-        CipherFactory factory = CipherFactory.getInstance();
-        this.cipher = factory.get(CipherFactory.Algorithm.AES);
-    }
-
+    /**
+     * Test with the master key and vector salt has input length divisible by 8.
+     */
     @Test
     public void checkKeyAndSaltInputLength() {
         Assert.assertEquals(0, KEY.length % 8);
         Assert.assertEquals(0, SALT.length % 8);
     }
 
+    /**
+     * When we encrypt data with AES algorithm for 16 inputs length, then we expect decrypt data are the same.
+     *
+     * @throws CipherException Throw when we've problems to instantiate {@link ICipher} or encrypt data.
+     */
     @Test
-    public void whenEncryptData16_thenDecryptSameData() throws CipherException {
+    public void whenAESEncryptData16_thenDecryptSameData() throws CipherException {
+        CipherFactory factory = CipherFactory.getInstance();
+        ICipher cipherAES = factory.get(CipherFactory.Algorithm.AES);
+
         byte[] data = CREDIT_CARD_NUMBER_16.getBytes(StandardCharsets.UTF_8);
-        byte[] encryptedData = this.cipher.encrypt(KEY, SALT, data);
+        byte[] encryptedData = cipherAES.encrypt(KEY, SALT, data);
         Assert.assertEquals(0, encryptedData.length % 16);
 
-        byte[] decryptedData = this.cipher.decrypt(KEY, SALT, encryptedData);
+        byte[] decryptedData = cipherAES.decrypt(KEY, SALT, encryptedData);
         Assert.assertArrayEquals(data, decryptedData);
     }
 
+    /**
+     * When we encrypt data with AES algorithm for 22 inputs length, then we expect decrypt data are the same.
+     *
+     * @throws CipherException Throw when we've problems to instantiate {@link ICipher} or encrypt data.
+     */
     @Test
-    public void whenEncryptData22_thenDecryptSameData() throws CipherException {
+    public void whenAESEncryptData22_thenDecryptSameData() throws CipherException {
+        CipherFactory factory = CipherFactory.getInstance();
+        ICipher cipherAES = factory.get(CipherFactory.Algorithm.AES);
+
         byte[] data = CREDIT_CARD_NUMBER_22.getBytes(StandardCharsets.UTF_8);
-        byte[] encryptedData = this.cipher.encrypt(KEY, SALT, data);
+        byte[] encryptedData = cipherAES.encrypt(KEY, SALT, data);
         Assert.assertEquals(0, encryptedData.length % 16);
 
-        byte[] decryptedData = this.cipher.decrypt(KEY, SALT, encryptedData);
+        byte[] decryptedData = cipherAES.decrypt(KEY, SALT, encryptedData);
         Assert.assertArrayEquals(data, decryptedData);
     }
 
